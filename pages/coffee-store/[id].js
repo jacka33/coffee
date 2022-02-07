50454f0fe9fc27356191952914dd9624bd519834
pilotjack <jack@intercountry.com>
2022-02-07 18:52:29 +0100

    dynamic link

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -7,6 +7,8 @@ const CoffeeStore = () => {
         <div>
             {router.query.id}
             <Link href="/"><a>Back to home</a></Link>
+            <Link href="/coffee-store/dynamic"><a>Go to dynamic</a></Link>
+
         </div>
     )
 }
