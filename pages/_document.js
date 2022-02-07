0f9819e198d57b7880703d0805298e7d734024b9
pilotjack <jack@intercountry.com>
2022-02-07 20:35:04 +0100

    font links

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,10 @@ class MyDocument extends Document {
         return (
             <Html lang="en">
                 <Head>
+                    <link rel="preload" href="/fonts/IBMPlexSans-Bold.ttf" as="font" crossOrigin="anonymous" />
+                    <link rel="preload" href="/fonts/IBMPlexSans-Regular.ttf" as="font" crossOrigin="anonymous" />
+                    <link rel="preload" href="/fonts/IBMPlexSans-SemiBold.ttf" as="font" crossOrigin="anonymous" />
+
                 </Head>
                 <body>
                     <Main />
