26d659d995bb285336d3ed0e14096ad6ed496047
pilotjack <jack@intercountry.com>
2022-02-07 20:10:45 +0100

    add hero image

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <Banner buttonText="View stores nearby" handleOnClick={handleOnBannerBtnClick} />
-
+        <Image src="/static/hero-image.png" width={700} height={400} />
       </main>
 
     </div>
