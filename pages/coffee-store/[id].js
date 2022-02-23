3f2fa6b815b256a0c0b24c82b699e1fc6b93437d
pilotjack <jack@intercountry.com>
2022-02-23 10:35:02 +0100

    get static paths

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,13 +1,42 @@
 import Link from "next/link";
 import { useRouter } from "next/router"
 
-const CoffeeStore = () => {
+import coffeeStoresData from '../../data/coffee-stores.json'
+
+export function getStaticProps({ params }) {
+
+    return {
+        props: {
+            coffeeStore: coffeeStoresData.find(coffeeStore => {
+                return coffeeStore.id.toString() === params.id;
+            })
+        }
+    }
+}
+
+export function getStaticPaths() {
+    return {
+        paths: [
+            { params: { id: '0' } },
+            { params: { id: '1' } }
+        ],
+        fallback: true
+    }
+}
+
+const CoffeeStore = (props) => {
     const router = useRouter();
+
+    if (router.isFallback) {
+        return <div>Loading...</div>
+    }
     return (
         <div>
             {router.query.id}
             <Link href="/"><a>Back to home</a></Link>
             <Link href="/coffee-store/dynamic"><a>Go to dynamic</a></Link>
+            <p>{props.coffeeStore.address}</p>
+            <p>{props.coffeeStore.name}</p>
 
         </div>
     )
